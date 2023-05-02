609728ff1d0837d1e69909b2b879c85dcfa905cf
hammurapi <3584715+hammurapi@users.noreply.github.com>
2023-05-02 21:40:34 +0200

    Replacing logo adding config

--- extension.js
+++ extension.js
@@ -1,9 +1,16 @@
 'use strict'
 
+const vscode = require('vscode');
+
 function activate(context) {
     return {
         extendMarkdownIt(md) {
-            return md.use(require('markdown-it-include'));
+            const config = vscode.workspace.getConfiguration('markdown-include');
+
+            return md.use(require('markdown-it-include'), {
+                root: vscode.workspace.rootPath,
+                includeRe: new RegExp( config.get('includeRe') ),
+              } );
         }
     };
 }
